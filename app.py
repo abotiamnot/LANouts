06794db85f2ff609dcc9e92224a1958a3cb7b1b8
abotiamnot <yatharthrai16@ducic.ac.in>
2018-11-11 20:28:40 +0530

    Some more changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, g, redirect, flash, url_for, session, logging, request
-# from flask_socketio import SocketIO
+from flask import Flask, render_template, redirect, url_for, session, request
 import os
+from flask_socketio import SocketIO, emit
 
 lan_pass = input("Please input the LAN Pass for the session \n")
 sign_in = False
@@ -10,11 +10,13 @@ def find_local():
 	return subprocess.check_output("hostname -I", shell=True).decode('utf-8').split(' ')[0]
 
 app = Flask(__name__)
-# socketio = SocketIO(app)
+app.config['SECRET_KEY'] = os.urandom(36)
+socketio = SocketIO(app)
 
 @app.route('/')
 def main():
-	return redirect(url_for('signin'))
+    return redirect(url_for('signin'))
+
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
@@ -25,23 +27,22 @@ def signin():
 			return redirect(url_for('main_menu'))
 	return render_template('signin.html')
 
+@app.route('/chatroom', methods=['GET', 'POST'])
+def chatroom():
+    if 'sign_in' in session:
+        return render_template('chatroom.html',
+                               user = session['name'])
+    return redirect(url_for('signin'))
+
 @app.route('/mainmenu')
 def main_menu():
 	if 'sign_in' in session:
 		return render_template('main_menu.html')
 	return redirect(url_for('signin'))
 
-@app.route('/aboutus')
-def aboutus():
-	if 'sign_in' in session:
-		return render_template('aboutus.html')
-	return redirect(url_for('signin'))
-
-
-
 
 if __name__ == "__main__":
-	ip_ = find_local()
-	print("Please connect to the following IP Address: {}".format(ip_))
-	app.secret_key = os.urandom(36)
-	app.run(host='0.0.0.0', debug=True)
+    ip_ = find_local()
+    print("Please connect to the following IP Address: {}".format(ip_))
+    socketio.run(app, host='0.0.0.0', debug=True)
+	# # app.run(host='0.0.0.0', debug=True)
